b93d8b4ad8117eccf8f6efe9259282a3dd764b5f
Cassubian Detk <kontakt@detk.pl>
2018-02-25 19:45:22 +0100

    makef

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Kòmpilatór Dëtka. Sprôwdzóm ë jinstalëjã zanôleżnoscë."
+echo "Detk builder. Now checking and installing dependencies."
+echo "Kompilator Detka. Sprawdzam i instaluję zależności."
+wait 10
+sudo apt-get update
+sudo apt-get install build-essential autoconf g++ qt4-qmake libqt4-dev build-essential libssl-dev  git libssl-dev libtool pkg-config libevent-dev libminiupnpc-dev libleveldb-dev libqrencode-dev libqtgui4  libglib2.0-dev libncurses5-dev curl libjansson4 libjansson-dev -y
+sudo apt-get install libboost-all-dev libboost1.55-all-dev libdb++-dev libdb5.3++-dev libdb5.3-dev -y
+mv src/rpcrawtransaction.cpp src/rpcrawtransaction48.cpp
+mv src/rpcrawtransaction58.cpp src/rpcrawtransaction.cpp
+qmake
+make
